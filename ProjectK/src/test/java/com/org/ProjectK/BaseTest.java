1941eb447ddccadfdaa8b5715f17725fe51982f9
saipraneethkammela <saipraneeth.kammela@gmail.com>
2024-06-29 14:30:45 +0530

    First Commit

---
+++ ProjectK/src/test/java/com/org/ProjectK/BaseTest.java
@@ -0,0 +1,13 @@
+package com.org.ProjectK;
+
+import org.testng.annotations.Test;
+
+public class BaseTest
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("This is an example test");
+    }
+}
